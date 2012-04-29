b3188c6f8ee41c17fa3f0b887e779ee979a2aab2
Nicolas Williams <nico@cryptonector.com>
2012-04-28 21:32:17 -0500

    Closer to working

--- thread_safe_global.c
+++ thread_safe_global.c
@@ -1,60 +1,272 @@
 
-/*
- * See .h for description of algorithm.
- */
-
+#include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include "thread_safe_global.h"
+
+static void
+wrapper_free(struct cfwrapper *wrapper)
+{
+    if (wrapper == NULL)
+        return;
+    if (wrapper != NULL && atomic_dec_uint32(&wrapper->nref) == 0) {
+        if (wrapper->dtor != NULL)
+            wrapper->dtor(wrapper);
+        free(wrapper);
+    }
+}
+
+static void
+cfvar_dtor_wrapper(void *wrapper)
+{
+    wrapper_free(wrapper);
+}
 
 /**
  * Initialize a configuration variable
  *
+ * A configuration variable stores a current value, a pointer to void,
+ * which may be set and read.  A value read from a configuration
+ * variable will be valid in the thread that read it, and will remain
+ * valid until released or until the configuration variable is read
+ * again in the same thread.  New values may be set.  Values will be
+ * destroyed with the destructor provided when no references remain.
+ *
  * @param cfvar Pointer to configuration variable
- * @param get_ref Pointer to function that gets a reference to cf data
- * @param put_ref Pointer to function that releases a reference to cf data, destroying the configuration data on last release
- * @param cfdata Pointer to configuration data
+ * @param dtor Pointer to configuration value destructor function
+ *
+ * @return Returns zero on success, else a system error number
  */
-void pthread_cfvar_init_np(pthread_cfvar_np_t *cfvar,
-			   pthread_cfvar_ref_np_t get_ref,
-			   pthread_cfvar_ref_np_t put_ref,
-                           void *cfdata)
+int
+pthread_cfvar_init_np(pthread_cfvar_np_t *cfv,
+                      pthread_cfvar_destructor_np_t dtor)
 {
-    memset(cfvar, 0, sizeof (*cfvar));
-    /* Init mutexes, cond var */
+    int err;
+
+    if ((err = pthread_key_create(&cfv->tkey, cfvar_dtor_wrapper)) != 0)
+        return err;
+    if ((err = pthread_mutex_init(&cfv->write_lock, NULL)) != 0)
+        return err;
+    if ((err = pthread_mutex_init(&cfv->cv_lock, NULL)) != 0) {
+        pthread_mutex_destroy(&cfv->write_lock);
+        return err;
+    }
+    if ((err = pthread_cond_init(&cfv->cv, NULL)) != 0) {
+        pthread_mutex_destroy(&cfv->write_lock);
+        pthread_mutex_destroy(&cfv->cv_lock);
+        return err;
+    }
+    cfv->vars[0].nreaders = 0;
+    cfv->vars[0].wrapped_cf = NULL;
+    cfv->vars[0].other = &cfv->vars[1];
+    cfv->vars[1].nreaders = 0;
+    cfv->vars[1].wrapped_cf = NULL;
+    cfv->vars[1].other = &cfv->vars[0];
+    cfv->current = NULL;
+    cfv->dtor = dtor;
+    return 0;
 }
 
 /**
  * Destroy a configuration variable
+ *
+ * It is the caller's responsibility to ensure that no thread is using
+ * this cfvar and that none will use it again.
+ *
+ * @param [in] cfvar The configuration variable to destroy
  */
-void pthread_cfvar_destroy_np(pthread_cfvar_np_t *cfvar);
+void
+pthread_cfvar_destroy_np(pthread_cfvar_np_t *cfvar)
+{
+    if (cfvar == 0)
+        return;
+
+    pthread_cond_destroy(&cfvar->cv);
+    pthread_mutex_destroy(&cfvar->cv_lock);
+    pthread_mutex_destroy(&cfvar->write_lock);
+    /* CFs will be release by the thread-specifics' destructors */
+    /* We leak cfvar->tkey */
+}
 
 /**
  * Get the most up to date configuration data
  *
  * @param [in] cfvar Pointer to a cf var
+ * @param [out] res Pointer to location where configuration data will be output
  * @param [out] version Pointer to 64-bit integer where the current version will be output
  *
  * @return Pointer to configuration data
  */
-void *pthread_cfvar_get_np(pthread_cfvar_np_t *cfvar, uint64_t *version);
+int pthread_cfvar_init_np(pthread_cfvar_np_t *, pthread_cfvar_destructor_np_t);
+void pthread_cfvar_destroy_np(pthread_cfvar_np_t *);
+int pthread_cfvar_get_np(pthread_cfvar_np_t *, void **, uint64_t *);
+int
+pthread_cfvar_get_np(pthread_cfvar_np_t *cfv, void **res, uint64_t *version)
+{
+    int err;
+    size_t i;
+    struct cfvar *v;
+
+    *res = NULL;
+    *version = 0;
+    v = cfv->current;
+    if (v == NULL)
+        return 0;
+    (void) atomic_inc_uint32(&v->nreaders);
+    for (i = 2; i > 0; i--) {
+        if (v->wrapped_cf == NULL) {
+            /* Whoa, nothing there; shouldn't happen */
+            (void) atomic_dec_uint32(&v->nreaders);
+            return 0;
+        }
+        if (atomic_inc_uint32(&v->wrapped_cf->nref) > 1) {
+            /* Done */
+            if ((err = pthread_setspecific(cfv->tkey, v->wrapped_cf)) != 0)
+                return err;
+            *version = v->wrapped_cf->version;
+            *res = v->wrapped_cf->ptr;
+            return 0;
+        }
+        /*
+         * We're racing with a writer.
+         *
+         * We increment the reader count on the other v before
+         * decrementing the reader count on this one.  This should
+         * guarantee that we find the other one present if this one was
+         * being replaced.
+         */
+        atomic_inc_uint32(&v->other->nreaders);
+        if (atomic_dec_uint32(&v->nreaders) == 0) {
+            /*
+             * As the last reader of this v we have to signal the
+             * writer that may be waiting for us to stop reading.
+             */
+            if ((err = pthread_mutex_lock(&cfv->cv_lock)) != 0) {
+                atomic_dec_uint32(&v->other->nreaders);
+                return err;
+            }
+
+            wrapper_free(v->wrapped_cf);
+            v->wrapped_cf = NULL;
+
+            if ((err = pthread_cond_broadcast(&cfv->cv)) != 0) {
+                atomic_dec_uint32(&v->other->nreaders);
+                (void) pthread_mutex_unlock(&cfv->cv_lock);
+                return err;
+            }
+            if ((err = pthread_mutex_unlock(&cfv->cv_lock)) != 1) {
+                atomic_dec_uint32(&v->other->nreaders);
+                return err;
+            }
+        }
+        v = v->other;
+    }
+    return EAGAIN; /* shouldn't happen! */
+}
 
 /**
- * Release configuration data
+ * Release this thread's reference to the current value of the given
+ * configuration variable.
  *
- * @param cfvar Pointer to configuration variable
- * @param cfdata Pointer to configuration data
+ * @param cfv [in] A configuration variable
  */
-void pthread_cfvar_release_np(pthread_cfvar_np_t *cfvar, void *cfdata);
+void
+pthread_cfvar_release_np(pthread_cfvar_np_t *cfv)
+{
+    wrapper_free(pthread_getspecific(cfv->tkey));
+}
 
 /**
  * Set new data on a configuration variable
  *
  * @param [in] cfvar Pointer to configuration variable
- * @param [in] version Version number, must be 0 or current version + 1
  * @param [in] cfdata Pointer to configuration data
  * @param [out] new_version New version number
  *
- * @return 0 on success, EEXIST if there's a conflict, or a system error such as ENOMEM.
+ * @return 0 on success, or a system error such as ENOMEM.
  */
-int pthread_cfvar_set_np(pthread_cfvar_np_t *cfvar, uint64_t version,
-			 void *cfdata, uint64_t *new_version);
+int
+pthread_cfvar_set_np(pthread_cfvar_np_t *cfv, void *cfdata,
+                     uint64_t *new_version)
+{
+    int err;
+    size_t i;
+    struct cfvar *v;
+    struct cfwrapper *old_wrapper = NULL;
+    struct cfwrapper *wrapper;
+
+    *new_version = 0;
+
+    if ((wrapper = calloc(1, sizeof(*wrapper))) == NULL)
+        return errno;
+
+    /*
+     * Starting the new value with nref == 1 is critical.
+     *
+     * Readers take the fast path when they incred this and get
+     * something > 1.  Writers release this one reference when they
+     * replace the value in the old slot, and race with the last reader
+     * to actually free it (the race is serialized, naturally; the one
+     * who gets nref == 0 destroys the value).
+     */
+    wrapper->nref = 1;
+    wrapper->dtor = cfv->dtor;
+    wrapper->ptr = cfdata;
+
+    if ((err = pthread_mutex_lock(&cfv->write_lock)) != 0) {
+        free(wrapper);
+        return err;
+    }
+
+    wrapper->version = ++(cfv->version);
+
+    if (cfv->current == NULL) {
+        /* This is the first cf being written; easy */
+        v = &cfv->vars[0];
+        v->wrapped_cf = wrapper;
+        cfv->current = v;
+        return pthread_mutex_unlock(&cfv->write_lock);
+    }
+
+    /* Grab the other slot */
+    v = cfv->current->other;
+    old_wrapper = cfv->current->wrapped_cf;
+
+    /* Wait until that slot is quiescent before mutating it */
+    if ((err = pthread_mutex_lock(&cfv->cv_lock)) != 0) {
+        (void) pthread_mutex_unlock(&cfv->write_lock);
+        free(wrapper);
+        return err;
+    }
+    while (v->nreaders > 0) {
+        /*
+         * We have a separate lock for writing vs. waiting so that no
+         * other writer can steal our march.  We got here by winning the
+         * race for the writer lock, so we'll hold onto it, and thus
+         * avoid having to restart here.
+         */
+        if ((err = pthread_cond_wait(&cfv->cv, &cfv->cv_lock)) != 0) {
+            (void) pthread_mutex_unlock(&cfv->cv_lock);
+            (void) pthread_mutex_unlock(&cfv->write_lock);
+            free(wrapper);
+            return err;
+        }
+    }
+    if ((err = pthread_mutex_unlock(&cfv->cv_lock)) != 0) {
+        (void) pthread_mutex_unlock(&cfv->write_lock);
+        free(wrapper);
+        return err;
+    }
+
+    /* Update that now quiescent slot */
+    v->wrapped_cf = wrapper;
+    cfv->current = v;       /* new cf now visible */
+
+    /* Done */
+    err = pthread_mutex_unlock(&cfv->write_lock);
+
+    /* Release the old cf */
+    wrapper_free(old_wrapper);
+    return err;
+}
 
--- thread_safe_global.h
+++ thread_safe_global.h
@@ -1,174 +1,63 @@
 
-/*
- * This is an attempt at a MT-hot "current configuration" API with
- * thread-safe updates.  Readers do nothing that blocks except for the
- * last reader when there's a waiting writer, in which case the reader
- * grabs a lock, signals the writer, then drops the lock and goes on its
- * merry way.  This isn't just for configuration though: it could be
- * used for lots of things, such as the global table of string constants
- * in libheimbase (see the HSTR() macro used in libheimbase).
- *
- * This could also be used for a COW approach to global state.  Suppose
- * you want to update something deep in global state, suppose in fact
- * that you have a tree of data using something like libheimbase objects
- * (or Core Foundation objects), and you want to update a leaf object
- * using an XPath-like API.  But this being global state means you need
- * synchronization.  If the XPath-like API includes a COW version that
- * returns a new root then you can just use this API here to replace the
- * global root.
- *
- * This is a better interface than reader/writer locks.  There can be no
- * writer starvation.  Readers mostly do not block.  Only the last
- * reader of a given version of configuration data can block, and only
- * at times when there are two or more writes in quick succession.
- * And such a reader it will wait only for the one active writer, if
- * there is one.
- *
- * If multiple readers race with a writer some may get the configuration
- * data that was in effect before the writer updates it, and some may
- * get the configuration data that the writer sets.  But once a writer
- * is [almost] finished then all readers will get the new configuration
- * data until the next writer comes along.
- *
- * See Doxygen comments below for API details.
- *
- * There is no guarantee that by the time the 'set' function returns all
- * other threads are using the new configuration data.  The caller may
- * arrange to obtain such guarantees by acquiring a mutex before calling
- * the 'set' funciton then blocking on a condition variable that will be
- * signaled by the last reference release of the previous configuration.
- * The caller may need to arrange to wake up all threads (e.g., that are
- * waiting in an event loop) in order to make such a synchronization
- * mechanism timely.
- *
- * The way this works is that the cfvar holds two versions of
- * configuration data.  Readers will always get the newest version.
- * Writers replace what used to be the oldest version.  See the struct
- * definition below this comment block, then the algorithm immediately
- * below:
- *
- * To set a new ptr for a pthread_cfvar_np_t variable named foo we:
- *   acquire foo->write_lock; (exclude other writers)
- *   acquire foo->cv_lock; (used for waiting for last reader of old config)
- *   atomic increment foo->writer;
- *
- *   find x such that foo->vers[x].vers < foo->vers[(x + 1) % 2].vers;
- *   old_ptr = foo->vers[x].ptr;
- *   foo->vers[x].ptr = new_ptr;
- *   foo->vers[x].vers = foo->vers[(x + 1) % 2].vers + 1;
- *   producer membar;
- *   while foo->vers[(x + 1) % 2].readers > 0
- *     cond_wait(foo->cv, foo->cv_lock);
- *   atomic decrement foo->writer;
- *   release foo->cv_lock;
- *   release foo->write_lock;
- *   foo->put_ref(old_ptr);
- *   return;
- *
- * To get the newest available ptr from foo:
- *   atomic increment foo->vers[0].readers;
- *   atomic increment foo->vers[1].readers;
- *   membar_consumer;
- *   find x such that foo->vers[x].vers > foo->vers[(x + 1) % 2].vers;
- *   atomic decrement foo->[(x + 1) % 2].readers;
- *   ptr = foo->vers[x].ptr;
- *   foo->get_ref(ptr);
- *   if (atomic decrement foo->[x].readers now zero &&
- *       atomic_read(foo->writer)
- *     membar_consumer;
- *     acquire foo->cv_lock; (if racing with a writer this is where we
- *                            serialize w.r.t. them)
- *     cond_signal(foo->cv); (if there's no writer waiting, no sweat)
- *     drop foo->cv_lock;
- *   return ptr;
- *
- * Note that no writer starvation is possible because the last reader of
- * an old configuration data version will notice that it is the last
- * reader, and will signal the waiting writer, and new readers will be
- * getting the new version if they start after the writer updates the
- * new version, thus new readers can't block the waiting writer.
- * Writers only ever block on the last reader of the old configuration.
- * Why would there be a reader getting the old config?  Well, 
- *
- * Note that readers can notice a version change that happened after
- * their selection of a version; readers could re-start to get the new
- * version, but this might cause problems (e.g., readers never finishing
- * because of a constant stream of writers).
- *
- * What's missing?  Ah, we need to pthread_cancel_push/pop() around the
- * bodies of the get and set functions so that we can avoid leaving
- * cfvars in bad state, or leak references to cf data when the thread
- * gets cancelled.  The caller is responsible for doing the same to
- * avoid leaking references to cf data, but most apps don't bother with
- * this -- we have to because we aim to be a library utility.
- */
+#ifndef URCU_H
 
-typedef void (*pthread_cfvar_ref_np_t)(void *);
+#include <sys/types.h>
+#include <stdint.h>
+#include <pthread.h>
 
-struct cfvar_vers {
-    AO_t	vers;
-    AO_t	readers;
-    void	*ptr;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* _np == non-portable, really, non-standard extension */
+typedef void (*pthread_cfvar_destructor_np_t)(void *);
+
+/* XXX Make private? */
+struct cfwrapper {
+    pthread_cfvar_destructor_np_t   dtor;
+    uint32_t                        nref;
+    uint64_t                        version;
+    void                            *ptr;
+};
+
+struct cfvar {
+    uint32_t            nreaders;
+    struct cfwrapper    *wrapped_cf; // has nref, wraps real ptr
+    struct cfvar        *other;
 };
 
 typedef struct pthread_cfvar_np {
-    pthread_mutex_t		write_lock;
-    pthread_mutex_t		cv_lock;
-    pthread_cond_t		cv;
-    pthread_cfvar_ref_np_t	get_ref;
-    pthread_cfvar_ref_np_t	put_ref;
-    AO_t			writer;
-    struct cfvar_vers		vers[2];
+    pthread_key_t                   tkey;
+    pthread_mutex_t                 write_lock; /* one writer at a time */
+    pthread_mutex_t                 cv_lock;    /* to signal waiting writer */
+    pthread_cond_t                  cv;         /* to signal waiting writer */
+    struct cfvar                    *current;   /* both read; writer writes */
+    struct cfvar                    vars[2];    /* writer only */
+    pthread_cfvar_destructor_np_t   dtor;       /* both read this */
+    uint64_t                        version;    /* writer only */
 } pthread_cfvar_np_t;
 
+/**
+ * A pthread_cfvar_np_t is an object that holds a pointer to what would
+ * typically be configuration information.  Threads can safely read and
+ * write this via pthread_cfvar_get_np() and pthread_cfvar_set_np(),
+ * with configuration values destroyed only when the last reference is
+ * released.
+ *
+ * Writes are serialized.  Readers don't block, and mostly perform only
+ * fast atomic operations; the only blocking operations done by readers
+ * are for uncontended resources.
+ */
 typedef struct pthread_cfvar_np pthread_cfvar_np_t;
 
-/**
- * Initialize a configuration variable
- *
- * @param cfvar Pointer to configuration variable
- * @param get_ref Pointer to function that gets a reference to cf data
- * @param put_ref Pointer to function that releases a reference to cf data, destroying the configuration data on last release
- * @param cfdata Pointer to configuration data
- */
-void pthread_cfvar_init_np(pthread_cfvar_np_t *cfvar,
-			   pthread_cfvar_ref_np_t get_ref,
-			   pthread_cfvar_ref_np_t put_ref,
-                           void *cfdata);
+int pthread_cfvar_init_np(pthread_cfvar_np_t *, pthread_cfvar_destructor_np_t);
+void pthread_cfvar_destroy_np(pthread_cfvar_np_t *);
+int pthread_cfvar_get_np(pthread_cfvar_np_t *, void **, uint64_t *);
+void pthread_cfvar_release_np(pthread_cfvar_np_t *);
+int pthread_cfvar_set_np(pthread_cfvar_np_t *, void *, uint64_t *);
 
-/**
- * Destroy a configuration variable
- */
-void pthread_cfvar_destroy_np(pthread_cfvar_np_t *cfvar);
+#ifdef __cplusplus
+}
+#endif
 
-/**
- * Get the most up to date configuration data
- *
- * @param [in] cfvar Pointer to a cf var
- * @param [out] version Pointer to 64-bit integer where the current version will be output
- *
- * @return Pointer to configuration data
- */
-void *pthread_cfvar_get_np(pthread_cfvar_np_t *cfvar, uint64_t *version);
-
-/**
- * Release configuration data, setting NULL as the new value
- *
- * @param cfvar Pointer to configuration variable
- * @param cfdata Pointer to configuration data
- */
-void pthread_cfvar_release_np(pthread_cfvar_np_t *cfvar, void *cfdata);
-
-/**
- * Set new data on a configuration variable
- *
- * @param [in] cfvar Pointer to configuration variable
- * @param [in] version Version number, must be either 0 or current version + 1
- * @param [in] cfdata Pointer to configuration data
- * @param [out] new_version New version
- *
- * @return 0 on success, EEXIST if there's a conflict, or a system error such as ENOMEM.
- */
-int pthread_cfvar_set_np(pthread_cfvar_np_t *cfvar, uint64_t version,
-			 void *cfdata, uint64_t *new_version);
-
+#endif /* URCU_H */
