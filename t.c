41d13b817fd1d6c68d18843311956605ec6d1fd9
Nicolas Williams <nico@cryptonector.com>
2015-12-27 18:44:55 -0600

    Print perf numbers in test

--- t.c
+++ t.c
@@ -9,8 +9,47 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
 #include "thread_safe_global.h"
+#include "atomics.h"
+
+/*
+ * TODO:
+ *  - Gather and print performance numbers
+ *
+ *    In particular:
+ *
+ *     - start time
+ *     - end time
+ *     - number of reads done
+ *     - number of reads / unit of time
+ *     - estimated time spent sleeping vs. running for each writer
+ *     - estimated time per write
+ */
+
+#define timeadd(a, b, result)                           \
+  do {                                                  \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;       \
+    (result)->tv_nsec = (a)->tv_nsec + (b)->tv_nsec;    \
+    if ((result)->tv_nsec >= 1000000000)                \
+      {                                                 \
+        ++(result)->tv_sec;                             \
+        (result)->tv_nsec -= 1000000000;                \
+      }                                                 \
+  } while (0)
+
+
+#define timesub(a, b, result)                           \
+  do {                                                  \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;       \
+    (result)->tv_nsec = (a)->tv_nsec - (b)->tv_nsec;    \
+    if ((result)->tv_nsec < 0) {                        \
+      --(result)->tv_sec;                               \
+      (result)->tv_nsec += 1000000000;                  \
+    }                                                   \
+  } while (0)
+
 
 static void *reader(void *data);
 static void *writer(void *data);
@@ -26,6 +65,10 @@ static pthread_cond_t exit_cv = PTHREAD_COND_INITIALIZER;
 static uint32_t nthreads = MY_NTHREADS;
 static uint32_t random_bytes[MY_NTHREADS];
 static uint64_t *runs[MY_NTHREADS];
+static struct timespec starttimes[MY_NTHREADS];
+static struct timespec endtimes[MY_NTHREADS];
+static struct timespec runtimes[MY_NTHREADS];
+static struct timespec sleeptimes[MY_NTHREADS];
 
 enum magic {
     MAGIC_FREED = 0xABADCAFEEFACDABAUL,
@@ -42,13 +85,19 @@ main(int argc, char **argv)
     int urandom_fd;
     uint64_t *magic_exit;
     uint64_t version;
+    struct timespec starttime;
+    struct timespec endtime;
+    struct timespec runtime;
+    struct timespec sleeptime;
+    uint64_t rruns = 0;
+    uint64_t wruns = 0;
 
     if ((magic_exit = malloc(sizeof(*magic_exit))) == NULL)
         err(1, "malloc failed");
     *magic_exit = MAGIC_EXIT;
     
     for (i = 0; i < MY_NTHREADS; i++)
-        runs[i] = 0;
+        runs[i] = NULL;
 
     if ((errno = pthread_var_init_np(&var, dtor)) != 0)
         err(1, "pthread_var_init_np() failed");
@@ -68,6 +117,10 @@ main(int argc, char **argv)
         if ((errno = pthread_detach(readers[i])) != 0)
             err(1, "Failed to detach reader thread no. %ju", (uintmax_t)i);
     }
+
+    if (clock_gettime(CLOCK_MONOTONIC, &starttime) != 0)
+        err(1, "clock_gettime(CLOCK_MONOTONIC) failed");
+
     for (k = i, i = 0; i < NWRITERS; i++, k++) {
         if ((errno = pthread_create(&writers[i], NULL, writer, &random_bytes[k])) != 0)
             err(1, "Failed to create writer thread no. %ju", (uintmax_t)i);
@@ -84,8 +137,57 @@ main(int argc, char **argv)
             printf("\nTold readers to exit.\n");
         }
     }
+
+    if (clock_gettime(CLOCK_MONOTONIC, &endtime) != 0)
+        err(1, "clock_gettime(CLOCK_MONOTONIC) failed");
+
+    timesub(&endtime, &starttime, &runtime);
+
     (void) pthread_mutex_unlock(&exit_cv_lock);
     pthread_var_destroy_np(var);
+
+    printf("Run time: %jus, %juns\n", (uintmax_t)runtime.tv_sec,
+           (uintmax_t)runtime.tv_nsec);
+
+    runtime.tv_sec = 0;
+    runtime.tv_nsec = 0;
+    sleeptime.tv_sec = 0;
+    sleeptime.tv_nsec = 0;
+    for (i = 0; i < NREADERS; i++) {
+        timeadd(&runtime, &runtimes[i], &runtime);
+        timeadd(&sleeptime, &sleeptimes[i], &sleeptime);
+        rruns += *(runs[i]);
+    }
+    printf("Read runs: %ju, read runtimes: %jus, %juns "
+           "read sleeptimes: %jus, %juns\n",
+           (uintmax_t)rruns,
+           (uintmax_t)runtime.tv_sec / NREADERS,
+           (uintmax_t)runtime.tv_nsec / NREADERS,
+           (uintmax_t)sleeptime.tv_sec / NREADERS,
+           (uintmax_t)sleeptime.tv_nsec / NREADERS);
+
+    runtime.tv_sec = 0;
+    runtime.tv_nsec = 0;
+    sleeptime.tv_sec = 0;
+    sleeptime.tv_nsec = 0;
+    for (i = 0; i < NWRITERS; i++) {
+        timeadd(&runtime, &runtimes[NREADERS + i], &runtime);
+        timeadd(&sleeptime, &sleeptimes[NREADERS + i], &sleeptime);
+        wruns += *(runs[NREADERS + i]);
+    }
+    printf("Write runs: %ju, write runtimes: %jus, %juns "
+           "write sleeptimes: %jus, %juns\n",
+           (uintmax_t)wruns,
+           (uintmax_t)runtime.tv_sec / NWRITERS,
+           (uintmax_t)runtime.tv_nsec / NWRITERS,
+           (uintmax_t)sleeptime.tv_sec / NWRITERS,
+           (uintmax_t)sleeptime.tv_nsec / NWRITERS);
+
+    printf("\n\n");
+
+    for (i = 0; i < MY_NTHREADS; i++)
+        free(runs[i]);
+
     return 0;
 }
 
@@ -96,8 +198,8 @@ reader(void *data)
     uint32_t i = *(uint32_t *)data;
     useconds_t us = i % 1000000;
     uint64_t version;
-    static __thread uint64_t last_version = 0;
-    static __thread uint64_t rruns = 0;
+    uint64_t last_version = 0;
+    uint64_t rruns = 0;
     void *p;
 
     runs[thread_num] = calloc(1, sizeof(runs[0]));
@@ -113,6 +215,9 @@ reader(void *data)
 
     if ((errno = pthread_var_wait_np(var)) != 0)
         err(1, "pthread_var_wait_np(var) failed");
+
+    if (clock_gettime(CLOCK_MONOTONIC, &starttimes[thread_num]) != 0)
+        err(1, "clock_gettime(CLOCK_MONOTONIC) failed");
 
     for (;;) {
         assert(rruns == (*(runs[thread_num])));
@@ -131,7 +236,7 @@ reader(void *data)
                 err(1, "Failed to signal exit cv");
             if ((errno = pthread_mutex_unlock(&exit_cv_lock)) != 0)
                 err(1, "Failed to release exit lock");
-            return NULL;
+            break;
         }
         assert(*(uint64_t *)p != MAGIC_FREED);
         assert(*(uint64_t *)p == MAGIC_INITED);
@@ -145,6 +250,22 @@ reader(void *data)
             (void) write(1, ".", sizeof(".")-1);
         usleep(us);
     }
+
+    if (clock_gettime(CLOCK_MONOTONIC, &endtimes[thread_num]) != 0)
+        err(1, "clock_gettime(CLOCK_MONOTONIC) failed");
+    assert(endtimes[thread_num].tv_sec != 0);
+
+    sleeptimes[thread_num].tv_sec = (us * rruns) / 1000000;
+    sleeptimes[thread_num].tv_nsec = ((us * rruns) % 1000000) * 1000;
+
+    timesub(&endtimes[thread_num],
+            &starttimes[thread_num],
+            &runtimes[thread_num]);
+    assert(runtimes[thread_num].tv_sec != 0);
+
+    runtimes[thread_num].tv_sec -= (us * rruns) / 1000000;
+    runtimes[thread_num].tv_nsec -= ((us * rruns) % 1000000) * 1000;
+
     return NULL;
 }
 
@@ -155,8 +276,8 @@ writer(void *data)
     uint32_t i = *(uint32_t *)data;
     useconds_t us = i % 1000000;
     uint64_t version;
-    static __thread uint64_t last_version = 0;
-    static __thread uint64_t wruns = 0;
+    uint64_t last_version = 0;
+    uint64_t wruns = 0;
     uint64_t *p;
 
     runs[thread_num] = calloc(1, sizeof(runs[0]));
@@ -175,6 +296,10 @@ writer(void *data)
 
     printf("Writer (%jd) will have %ju runs, sleeping %uus between\n", (intmax_t)thread_num - NREADERS, (uintmax_t)i, us);
     usleep(500000);
+
+    if (clock_gettime(CLOCK_MONOTONIC, &starttimes[thread_num]) != 0)
+        err(1, "clock_gettime(CLOCK_MONOTONIC) failed");
+
     for (; i > 0; i--) {
         assert(wruns == (*(runs[thread_num])));
         if ((p = malloc(sizeof(*p))) == NULL)
@@ -191,6 +316,21 @@ writer(void *data)
             (void) write(1, "-", sizeof("-")-1);
         usleep(us);
     }
+
+    if (clock_gettime(CLOCK_MONOTONIC, &endtimes[thread_num]) != 0)
+        err(1, "clock_gettime(CLOCK_MONOTONIC) failed");
+
+    sleeptimes[thread_num].tv_sec = (us * wruns) / 1000000;
+    sleeptimes[thread_num].tv_nsec = ((us * wruns) % 1000000) * 1000;
+
+    timesub(&endtimes[thread_num],
+            &starttimes[thread_num],
+            &runtimes[thread_num]);
+    assert(runtimes[thread_num].tv_sec != 0);
+
+    runtimes[thread_num].tv_sec -= (us * wruns) / 1000000;
+    runtimes[thread_num].tv_nsec -= ((us * wruns) % 1000000) * 1000;
+
     /*atomic_dec_32_nv(&nthreads);*/
     printf("\nWriter (%jd) exiting; threads left: %u\n", (intmax_t)thread_num, atomic_dec_32_nv(&nthreads));
     if ((errno = pthread_mutex_lock(&exit_cv_lock)) != 0)
